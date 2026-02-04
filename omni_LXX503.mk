e2993e52eb32a05ea6dad4601d3af3cb4a9bbe32
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2026-02-04 21:53:51 +0530

    Update omni_LXX503.mk

--- omni_LXX503.mk
+++ omni_LXX503.mk
@@ -1,9 +1,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from LXX503 device
