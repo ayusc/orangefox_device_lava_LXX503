c87709fc4923941b7fd72999b3c235f0a3ed8ac2
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2026-02-07 08:27:00 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,9 @@ PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 PLATFORM_SECURITY_PATCH := 2099-12-31
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+#https://source.android.com/docs/core/architecture/vintf/dm (race condition fix)
+DEVICE_MANIFEST_FILE := device/lava/LXX503/recovery/root/vendor/etc/vintf/manifest.xml
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
