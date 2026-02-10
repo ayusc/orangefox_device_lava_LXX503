5f0dc609176d4e9bff73f4abeb7bc4a6c7f944ac
ayusc <ayuschatterjee2003@gmail.com>
2026-02-11 00:06:02 +0530

    .

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,7 @@ TW_DEFAULT_BRIGHTNESS := 1024
 TW_MAX_BRIGHTNESS := 4095
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_PREPARE_DATA_MEDIA_EARLY := true
+TW_EXCLUDE_NANO := true
 #TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 #TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone4/temp
 #TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
