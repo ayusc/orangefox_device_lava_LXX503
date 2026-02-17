298b2a0b97edc94d66ac09e25039b74fe9d93b0c
Sergey Karbainov <karbainovs@mail.ru>
2026-02-17 14:57:19 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,8 +126,10 @@ OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
 OF_LOOP_DEVICE_ERRORS_TO_LOG := 1
 OF_DEFAULT_KEYMASTER_VERSION := 4.1
 #OF_FL_PATH1 := /sys/class/flashlight_core/flashlight/flashlight_torch
-FOX_COMPRESS_EXECUTABLES := 1
+FOX_COMPRESS_EXECUTABLES := /sbin /system/bin /vendor/bin
 FOX_DELETE_MAGISK_ADDON := 1
+OF_USE_MAGISKBOOT := 0
+
 # Filesystem Feature
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_FUSE_EXFAT := true
